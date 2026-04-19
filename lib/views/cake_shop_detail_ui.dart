c09867ef43113fd23a360ded83f18781f08f8e53
bzcom8192 <bzcom8192@gmail.com>
2026-04-19 12:02:01 +0700

    Add CakeShop model and implement cake shop listing and detail UI

--- lib/views/cake_shop_detail_ui.dart
+++ lib/views/cake_shop_detail_ui.dart
@@ -1,15 +1,317 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cake_shop_app/models/cake_shop.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class CakeShopDetailUi extends StatefulWidget {
-  const CakeShopDetailUi({super.key});
+  CakeShop? cakeShop;
+
+  CakeShopDetailUi({super.key, this.cakeShop});
 
   @override
   State<CakeShopDetailUi> createState() => _CakeShopDetailUiState();
 }
 
 class _CakeShopDetailUiState extends State<CakeShopDetailUi> {
+  Future<void> _makePhoneCall(String phoneNumber) async {
+    final Uri launchUri = Uri(
+      scheme: 'tel',
+      path: phoneNumber,
+    );
+    await launchUrl(launchUri);
+  }
+
+  Future<void> _launchInBrowser(Uri url) async {
+    if (!await launchUrl(
+      url,
+      mode: LaunchMode.externalApplication,
+    )) {
+      throw Exception('Could not launch $url');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(
+          widget.cakeShop!.name!,
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () => {
+            Navigator.pop(context),
+          },
+          icon: Icon(
+            Icons.arrow_back_ios_new,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: 40,
+            bottom: 50,
+            left: 35,
+            right: 35,
+          ),
+          child: Center(
+            child: Column(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  child: Image.asset(
+                    widget.cakeShop!.image1!,
+                    width: 120,
+                    height: 80,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 10,
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.asset(
+                        widget.cakeShop!.image2!,
+                        width: 120,
+                        height: 80,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.asset(
+                        widget.cakeShop!.image3!,
+                        width: 120,
+                        height: 80,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'ชื่อร้าน 🏬',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    widget.cakeShop!.name!,
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'เาลาเปิด-ปิด ⌚',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    widget.cakeShop!.openCloseTime!,
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'รายละเอียดร้าน 🗒️',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    widget.cakeShop!.description!,
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'ที่อยู่ร้าน 🚩',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    widget.cakeShop!.address!,
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () => {
+                    _makePhoneCall(widget.cakeShop!.phone!),
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                  ),
+                  child: Text(
+                    '📞 ${widget.cakeShop!.phone!}',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    FontAwesomeIcons.globe,
+                    color: Colors.yellow[700],
+                  ),
+                  title: Text(
+                    'เปิดเว็ปไซต์',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  trailing: Icon(
+                    Icons.open_in_new,
+                    color: Colors.blue,
+                  ),
+                  onTap: () {
+                    _launchInBrowser(Uri.parse(widget.cakeShop!.website!));
+                  },
+                ),
+                ListTile(
+                  leading: Icon(
+                    FontAwesomeIcons.facebook,
+                    color: Colors.blue[700],
+                  ),
+                  title: Text(
+                    'เปิด Facebook @${widget.cakeShop!.facebook!.replaceAll("https://www.facebook.com/", "")}',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  trailing: Icon(
+                    Icons.open_in_new,
+                    color: Colors.blue,
+                  ),
+                  onTap: () {
+                    _launchInBrowser(Uri.parse(widget.cakeShop!.facebook!));
+                  },
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 450,
+                  child: FlutterMap(
+                    options: MapOptions(
+                      initialCenter: LatLng(
+                        double.parse(widget.cakeShop!.latitude!),
+                        double.parse(widget.cakeShop!.longitude!),
+                      ),
+                      initialZoom: 16,
+                    ),
+                    children: [
+                      TileLayer(
+                        urlTemplate:
+                            'https://{s}.google.com/vt/lyrs=m,h&x={x}&y={y}&z={z}&hl=ar-MA&gl=MA',
+                        subdomains: const ['mt0', 'mt1', 'mt2', 'mt3'],
+                        userAgentPackageName: 'com.example.app',
+                      ),
+                      RichAttributionWidget(
+                        attributions: [
+                          TextSourceAttribution(
+                            'OpenStreetMap contributors',
+                            onTap: () {
+                              launchUrl(
+                                Uri.parse(
+                                    'https://openstreetmap.org/copyright'),
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                      // Marker
+                      MarkerLayer(
+                        markers: [
+                          Marker(
+                            child: InkWell(
+                              onTap: () {
+                                _launchInBrowser(
+                                  Uri.parse(
+                                    'https://www.google.com/maps/search/?api=1&query=${widget.cakeShop!.latitude},${widget.cakeShop!.longitude}',
+                                  ),
+                                );
+                              },
+                              child: Icon(
+                                Icons.location_on,
+                                color: Colors.red,
+                                size: 40,
+                              ),
+                            ),
+                            point: LatLng(
+                              double.parse(widget.cakeShop!.latitude!),
+                              double.parse(widget.cakeShop!.longitude!),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
